9886cff54e36c5cc8bcb00b12bfd6046b8db2bf3
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-10 18:29:11 +0200

    add style to pages content

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -4,12 +4,43 @@
 
 @section('content')
 
-    @foreach ($trains as $train)
-        <div class="d-flex">
-            <div>{{ $train->departure_station }} -</div>
-            <div>{{ $train->arrival_station }} -</div>
-            <div>{{ $train->departure_time }}</div>
-        </div>
-    @endforeach
+    <div class="container pt-5">
+
+        @foreach ($trains as $train)
+            <ul class="list-unstyled">
+                <li>
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-4">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $train->company }} - {{ $train->train_id }}</h5>
+                                    <div class="card-text train-logo">
+                                        {{ $train->is_deleted ? '🚫' : '🚄' }}
+                                    </div>
+                                    <p class="card-text">
+                                        <small class="text-muted">{{ $train->vagons_number }}</small>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $train->departure_station }} - {{ $train->arrival_station }}
+                                    </h5>
+                                    <ul class="card-text list-unstyled">
+                                        <li>Departure: {{ $train->departure_time }}</li>
+                                        <li>Arrival: {{ $train->arrival_time }}</li>
+                                    </ul>
+                                    <p class="card-text">
+                                        <small
+                                            class="text-muted">{{ $train->is_in_time ? 'The train is in perfect time' : 'The train will arrive a little late, sorry for the inconvenient' }}</small>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        @endforeach
+    </div>
 
 @endsection
